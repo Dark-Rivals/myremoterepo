5ea783a3c0e2508fcdc04d157868c52802dec9ac
Dark-Rivals <meera2002jawahar@gmail.com>
2023-06-01 14:00:43 +0530

    new modification for 2 files

--- A.java
+++ A.java
@@ -1,5 +1,6 @@
 public class A {
 	public static void main(String args[]) {
 		System.out.println("hello, 3rd change");
+		System.out.println("hello, 4rd change");
 	}
 }
--- B.java
+++ B.java
@@ -1,3 +1,6 @@
 public class B {
+public static void main(String args[]) {
+		System.out.println("hello, new B file");
+	}
 
 }
